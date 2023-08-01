1ba5bcbf7ba041713d8e41c0da0308dca66512cb
panaitandrei21 <panait.stefan2112@gmail.com>
2023-08-01 10:11:38 +0100

    allReactSTuff

---
+++ ReactPractice/superheroes-ui/src/App.js
@@ -0,0 +1,42 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import AllHeroes from './components/AllHeroes';
+import HeroForm from './components/HeroForm';
+import HeroDetail from './components/HeroDetail';
+
+
+
+
+
+function App() {
+  return (
+    <>
+    
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="/">Heroes</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/allheroes">All Heroes</Nav.Link>
+              <Nav.Link href="/addhero">Add Hero</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      
+        <Routes>
+          <Route path="allheroes" element={<AllHeroes/>} />
+          <Route path="addhero" element={<HeroForm/>} />
+          <Route path="/" element={<AllHeroes/>} />
+        </Routes>
+      
+    </>
+  );
+}
+
+export default App;
